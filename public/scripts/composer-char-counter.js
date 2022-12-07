be4d3bea30a47f2ce3ea98603adffbe98fe09d42
Sohaib-GO <s.almajmaie@gmail.com>
2022-12-06 18:41:15 -0700

    Fix issue
    Fix issue where the function might be triggered before doc load

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,18 @@
-// add event listener to the textarea
-$("textarea").on("input", function () {
-  // when the user types in the textarea, the event listener will be triggered
-  // the event listener will call the function that will count the number of characters
-  let counter = 140 - $(this).val().length;
-  // the function will update the counter with the number of characters
-  $(".counter").text(counter);
-  // if the number of characters is more than 140, the counter will turn red
-  if (counter < 0) {
-    $(".counter").css("color", "red");
-  } else {
-    // if the number of characters is less than 140, the counter will turn black
-    $(".counter").css("color", "black");
-  }
+/* eslint-disable no-undef */
+$(document).ready(function() {
+  // add event listener to the textarea
+  $("textarea").on("input", function() {
+    // when the user types in the textarea, the event listener will be triggered
+    // the event listener will call the function that will count the number of characters
+    let counter = 140 - $(this).val().length;
+    // the function will update the counter with the number of characters
+    $(".counter").text(counter);
+    // if the number of characters is more than 140, the counter will turn red
+    if (counter < 0) {
+      $(".counter").css("color", "red");
+    } else {
+      // if the number of characters is less than 140, the counter will turn black
+      $(".counter").css("color", "black");
+    }
+  });
 });
