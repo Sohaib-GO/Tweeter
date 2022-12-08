a0768a9e444ef314d85dfe20da7e0d4cffb7c2cd
Sohaib-GO <s.almajmaie@gmail.com>
2022-12-07 18:22:05 -0700

    Fetch tweets with Ajax
    -Remove the hard-coded tweets object that we added earlier in order to drive our renderTweets function
    - Fetch tweets with Ajax
    - Display the Time Passed Since a Tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,72 +1,77 @@
+/* eslint-disable no-undef */
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  const data = [];
 
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
+  const createTweetElement = function(tweet) {
+    const $tweet = $(`
+    <article class="tweet">
+    <header class="tweet-header">
+      <div class="tweet-pic">
+        <img src="${tweet.user.avatars}">
+      </div>
+      <div class="name">
+        <h4>${tweet.user.name}</h4>
+        <p>${tweet.user.handle}</p>
+      </div>
+      <div class="handle">
+        <p>${timeago.format(tweet.created_at)}</p>
+      </div>
+    </header>
+    <div class="tweet-body">
+      <p>${tweet.content.text}</p>
+    </div>
+    <footer class="tweet-footer">
+      <div class="icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    
+    </footer>
+    </article>
+    `);
+    return $tweet;
+  };
 
-const createTweetElement = function (tweet) {
-  const $tweet = $(`
-  <article class="tweet">
-  <header class="tweet-header">
-    <div class="tweet-pic">
-      <img src="${tweet.user.avatars}">
-    </div>
-    <div class="name">
-      <h2>${tweet.user.name}</h2>
-      <p>${tweet.user.handle}</p>
-    </div>
-    <div class="handle">
-      <p>${tweet.created_at}</p>
-    </div>
-  </header>
-  <div class="tweet-body">
-    <p>${tweet.content.text}</p>
-  </div>
-  <footer class="tweet-footer">
-    <div class="icons">
-      <i class="fa-solid fa-flag"></i>
-      <i class="fa-solid fa-retweet"></i>
-      <i class="fa-solid fa-heart"></i>
-    </div>
-  
-  </footer>
-  </article>
-  `);
-  return $tweet;
-};
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $(".tweetContainer").prepend($tweet);
+    }
+  };
 
-const renderTweets = function (tweets) {
-  for (let tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $(".tweetContainer").prepend($tweet);
-  }
-};
-
-$(document).ready(function () {
   renderTweets(data);
+
+  $("#tweet-form").submit(function(event) {
+    event.preventDefault();
+    const tweet = $(this).serialize();
+    console.log("tweet text", tweet);
+
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: tweet,
+    }).then(function(response) {
+      console.log("response ajax", response);
+    });
+  });
+
+  const loadTweets = function() {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+    })
+      //success callback function will simply call up renderTweets, passing it the response from the AJAX request.
+      .then(function(response) {
+        console.log("response ajax", response);
+        renderTweets(response);
+      });
+  };
+
+  loadTweets();
 });
